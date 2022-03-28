c303c47688cf8e8ba645e195da276a82726b3f4d
tonisj98 <antonio.serjime@gmail.com>
2022-03-28 09:38:49 +0200

    done

--- js/app.js
+++ js/app.js
@@ -4,3 +4,34 @@ $(document).ready(function () {
     console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
 
 });
+
+let cuenta = 0
+
+$("#sumar").click(function(){
+    cuenta++
+    console.log(cuenta)
+    $("#counter").text(cuenta)
+
+    if (cuenta > 0 ) {
+        $("#counter").css("color", "green");
+      }else {
+        $("#counter").css("color", "red");
+      }
+
+  });
+  
+  $("#restar").click(function(){
+    cuenta--
+    console.log(cuenta)
+    $("#counter").text(cuenta)
+
+    if (cuenta > 0 ) {
+        $("#counter").css("color", "green");
+      }else {
+        $("#counter").css("color", "red");
+      }
+
+  });
+
+
+  
